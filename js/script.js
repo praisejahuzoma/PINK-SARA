da40f816833f81bc31fa5d6e9876a847ff7e90fd
praisejahuzoma <praisejah.c.uzoma@gmail.com>
2021-09-13 17:40:27 +0100

    .

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+    
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu')
+
+// remove menu bar
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction() {
+    // active link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    // remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
